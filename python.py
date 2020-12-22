fd9f60054c64d1ae958b6bba0723fc22483d4f2e
Brendan Smith <shanghaied@gmail.com>
2020-12-22 16:31:30 -0600

    code: completed an if problem on the Hacker Rank

--- python.py
+++ python.py
@@ -39,3 +39,17 @@ i = 1
 
 for i in range(1, n+1):
     print(i, end="")
+
+######################################################
+
+n = 24
+
+if (n%2) == 0:
+    if 2 <= n <= 5:
+        print( 'Not Weird')
+    elif 6 <= n <= 20:
+        print( 'Weird' )
+    elif n > 20:
+        print( 'Not Weird')
+else:
+    print('Weird')
